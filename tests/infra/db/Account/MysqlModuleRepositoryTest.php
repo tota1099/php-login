4829b8e054900b330366539d4cfc1f0e222303d0
Renan Porto <renan.porto1099@gmail.com>
2020-12-22 20:17:31 -0300

    test: add MysqlModuleRepository test case: should return True if record exists

--- tests/infra/db/Account/MysqlModuleRepositoryTest.php
+++ tests/infra/db/Account/MysqlModuleRepositoryTest.php
@@ -20,4 +20,38 @@ final class MysqlModuleRepositoryTest extends TestCase
     $this->assertIsInt($module->id);
     $this->assertEquals($name, $module->name);
   }
+
+  public function testShouldThrowIfAlreadyExists() {
+    $name = $this->faker->name();
+    $addModuleModel = new AddModuleModel($name);
+    $module = $this->sut->add($addModuleModel);
+
+    $this->assertIsInt($module->id);
+    $this->assertEquals($name, $module->name);
+
+    $this->expectException(DomainError::class);
+    $this->expectExceptionMessage('Duplicate entry');
+    $this->sut->add($addModuleModel);
+  }
+
+  public function testShouldReturnTrueIfExists() {
+    $name = $this->faker->name();
+    $addModuleModel = new AddModuleModel($name);
+    $module = $this->sut->add($addModuleModel);
+
+    $this->assertTrue($this->sut->exists('id', $module->id));
+    $this->assertTrue($this->sut->exists('name', $module->name));
+  }
+
+  public function testShouldReturnFalseIfExists() {
+    $name = $this->faker->name();
+    $addModuleModel = new AddModuleModel($name);
+    $module = $this->sut->add($addModuleModel);
+
+    $this->assertTrue($this->sut->exists('id', $module->id));
+    $this->assertTrue($this->sut->exists('name', $module->name));
+
+    $this->assertFalse($this->sut->exists('id', $module->id + 1));
+    $this->assertFalse($this->sut->exists('name', $this->faker->name()));
+  }
 }
