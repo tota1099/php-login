a8bfcdc2e0b6a7c456c01eacdebcaf4ef59830f8
Renan Porto <renan.porto1099@gmail.com>
2021-03-04 21:02:45 -0300

    tests: Add FilterEmailValidatorTest

---
+++ tests/utils/FIlterEmailValidatorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class FilterEmailValidatorTest extends TestCase
+{
+  private EmailValidator $sut;
+
+  public function setUp(): void {
+    $this->sut = new FilterEmailValidator();
+  }
+
+  public function testShouldReturnTrueWithValidEmails() {
+    $this->assertTrue($this->sut->isValid('email@email.com'));
+    $this->assertTrue($this->sut->isValid('email@email.com'));
+    $this->assertTrue($this->sut->isValid('email-email@email.com'));
+  }
+
+  public function testShouldReturnFalseWithInvalidEmails() {
+    $this->assertFalse($this->sut->isValid('email-email.com'));
+    $this->assertFalse($this->sut->isValid('email@email'));
+    $this->assertFalse($this->sut->isValid('email@emailcom'));
+    $this->assertFalse($this->sut->isValid('email'));
+  }
+}
