dd55b38473a8a4a844efd6e91bef831895bf6d15
Renan Porto <renan.porto1099@gmail.com>
2021-03-08 19:17:42 -0300

    refactoring: ajustando factories para utilizar namespace

--- public/api/AddModule.php
+++ public/api/AddModule.php
@@ -2,4 +2,7 @@
 
 require_once(__DIR__ . '/../../config.php');
 
+use App\main\factories\controllers\AddModuleFactory;
+use App\main\helpers\RouteHelper;
+
 RouteHelper::route(AddModuleFactory::build());
--- public/api/SignUp.php
+++ public/api/SignUp.php
@@ -2,4 +2,7 @@
 
 require_once(__DIR__ . '/../../config.php');
 
+use App\main\factories\controllers\SignUpFactory;
+use App\main\helpers\RouteHelper;
+
 RouteHelper::route(SignUpFactory::build());
